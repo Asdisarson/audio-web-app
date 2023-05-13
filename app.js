591b9098e4572341a8e8680ce93401b72ff425c7
egill <egill@islandsvefir.is>
2023-05-13 10:26:30 +0000

    changes

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 $(document).ready(function () {
-  const audioFileUrl = './audio.m4a';
+  const audioFileUrl = ['./audio0.m4a', './audio1.m4a', './audio2.m4a', './audio3.m4a', './audio4.m4a'];
   const socket = io();
-  const sound = new Howl({ src: [audioFileUrl] });
+  const sound = [new Howl({ src: [audioFileUrl[0]] }),
+  new Howl({ src: [audioFileUrl[1]] }),
+  new Howl({ src: [audioFileUrl[2]] }),
+  new Howl({ src: [audioFileUrl[3]] }),
+  new Howl({ src: [audioFileUrl[4]] })];
 
   const isAdmin = window.location.search.includes('admin=true');
   if (isAdmin) {
+    $('.container').hide();
     $('#adminDashboard').show();
     socket.emit('adminConnected');
-    socket.on('updateUserList', (users) => {
-      const userList = $('#userList');
-      userList.empty();
-      for (const [userId, userData] of Object.entries(users)) {
-        userList.append(`<li>User ID: ${userId} | Latency: ${userData.latency} ms</li>`);
-      }
-    });
+    $('#gMinor').on('click', () => {
+      socket.emit('gMinor');
 
-    $('#startPlayback').on('click', () => {
-      socket.emit('startPlayback');
+    });
+    $('#soundsOff').on('click', () => {
+      socket.emit('soundsOff');
+
+    });
+    $('#random').on('click', () => {
+      socket.emit('random');
+
+    });
+    $('#play').on('click', () => {
+      socket.emit('play');
+
     });
   }
   else {
@@ -39,36 +49,57 @@ $(document).ready(function () {
 
       xhr.send();
     };
+    for (let i = 0; i < audioFileUrl.length; i++) {
 
-    downloadFile(
-      audioFileUrl,
-      (progress) => {
-        $('#progress').css('width', progress + '%');
-        if (progress === 100) {
-          $('#downloadStatus').text('Download completed. Click Connect to start.');
-          $('#connect').show();
-          $('#preloader').hide();
+
+      downloadFile(
+        audioFileUrl[i],
+        (progress) => {
+          $('#progress').css('width', progress + '%');
+          if (progress === 100 && ((i + 1) === audioFileUrl.length)) {
+            $('#downloadStatus').text('Download completed. Click Connect to start.');
+            $('#connect').show();
+            $('#preloader').hide();
+          }
+        },
+        (response) => {
+          sound[i].load();
         }
-      },
-      (response) => {
-        sound.load();
-      }
-    );
-
-    const userId = 'user_' + Math.random().toString(36).substr(2, 9);
+      );
+    }
     $('#connect').on('click', () => {
-      const latency = Math.floor(Math.random() * 1000); // Generate a random latency value for demonstration purposes
-      socket.emit('clientConnected', { userId, latency });
-    });
-    // Connect to the server and listen for the 'play' event
-    $('#connect').on('click', () => {
-
-      socket.on('play', () => {
-        sound.play();
+      socket.emit('clientConnected');
+      // Connect to the server and listen for the 'play' event
+      $('#connected').show();
+      $('#connect').hide();
+      socket.on('gMinor', () => {
+        // Randomly select a note from G-minor scale within the specified frequency range
+        const note = randomNoteFromGMinor();
+        synth.triggerAttackRelease(note, "8n");
       });
 
-      socket.emit('clientConnected');
+      socket.on('soundsOff', () => {
+        for (let i = 0; i < sound.length; i++) {
+          sound[i].stop();
+
+        }
+        // Stop all sounds
+        synth.triggerRelease();
+      });
+
+      socket.on('play', () => {
+        var i = Math.floor(Math.random() * 5);
+        // Play the random sound from the soundbank
+        sound[i].play();
+      });
+
+      socket.on('random', () => {
+        var i = Math.floor(Math.random() * 5);
+        // Play the random sound from the soundbank
+        sound[i].play();
+      });
     });
+
 
   }
 });
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const readline = require('readline');
 const os = require('os');
 
 const app = express();
@@ -9,38 +8,6 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 app.use(express.static('./'));
-const connectedUsers = {};
-let clientsReady = 0;
-
-io.on('connection', (socket) => {
-  socket.on('adminConnected', () => {
-    console.log('Admin connected');
-    socket.emit('updateUserList', connectedUsers);
-  });
-
-  console.log('A user connected');
-  socket.on('startPlayback', () => {
-    console.log('Admin started playback');
-    io.emit('play');
-  });
-
-  socket.on('clientConnected', (data) => {
-    const { userId, latency } = data;
-    connectedUsers[userId] = { latency };
-    console.log(`Client connected and ready to play audio. User ID: ${userId}`);
-    io.emit('updateUserList', connectedUsers);
-  });
-
-  socket.on('disconnect', () => {
-    if (connectedUsers.hasOwnProperty(socket.id)) {
-      delete connectedUsers[socket.id];
-      io.emit('updateUserList', connectedUsers);
-    }
-    console.log('A user disconnected');
-  });
-
-
-});
 function getLocalIP() {
   const interfaces = os.networkInterfaces();
   for (const ifaceName in interfaces) {
@@ -58,4 +25,50 @@ const localIP = getLocalIP();
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://${localIP}:${PORT}`);
+});
+
+io.on('connection', (socket) => {
+
+  socket.on('adminConnected', () => {
+    console.log('Admin')
+  });
+
+  // Handle new admin events
+  socket.on('gMinor', () => {
+    io
+    .emit('gMinor');
+  });
+
+  socket.on('soundsOff', () => {
+    io.emit('soundsOff');
+  });
+  socket.on('random', () => {
+    console.log(io.sockets.connected)
+    // Get an array of all connected socket IDs
+    const socketIds = Object.keys(io.sockets.connected);
+  
+    for (let i = 0; i < socketIds.length; i++) {
+      // Get the socket object for the current client
+      const socket = io.sockets.connected[socketIds[i]];
+      
+      // Emit the 'random' event to the current client
+      socket.emit('random');
+    }
+  });
+  
+
+  socket.on('play', () => {
+    io.emit('play');
+  });
+
+
+  socket.on('clientConnected', () => {
+    console.log(`Client connected and ready to play audio.`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+
 });
