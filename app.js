c1b55781f026d9d7479234d179910cb4907839ea
egill <egill@islandsvefir.is>
2023-05-16 21:55:11 +0000

    done

--- app.js
+++ app.js
@@ -1,154 +1,246 @@
 $(document).ready(function () {
     var buttonValue; // Declare the variable
     let audioFileUrl = './test.m4a';
-    const soundBank = ['./audio/audio0.m4a', './audio/audio1.m4a', './audio/audio2.m4a', './audio/audio3.m4a', './audio/audio4.m4a']
+    let soundBank = ['./audio/audio0.m4a', './audio/audio1.m4a', './audio/audio2.m4a']
     const urlParams = new URLSearchParams(window.location.search);
     const isAdmin = urlParams.get('admin');
-
-    $('#color .gameboy-button').on('click', function() {
+    let test = urlParams.get('test');
+    if (test === 'true') {
+        soundBank = ['./audio/test1.m4a', './audio/test.m4a', './audio/test2.m4a']
+    }
+    const socket = io();
+    $('#color .gameboy-button').on('click', function () {
         buttonValue = $(this).val(); // Set the variable to the value of the clicked button
         audioFileUrl = soundBank[buttonValue];
-        const socket = io();
         $('#preloader').show();
         $('#color').hide();
         if (isAdmin === 'true') {
-            audioFileUrl = "./audio/audio.m4a"
+            audioFileUrl = "./audio/audio.m4a";
+            if (test === 'true') {
+                audioFileUrl = "./audio/test3.m4a"
+            }
             socket.emit('isAdmin');
         }
-    // Get the URL parameters
+        // Get the URL parameters
 
 
 // Retrieve the value of the "admin" parameter
 
 // Check if the "admin" parameter is set to true
 
-    console.log(audioFileUrl);
-    const sound = new Howl({src: [audioFileUrl]});
-
-    // Preload audio file
-    const downloadFile = (url, onProgress, onComplete) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'blob';
-
-        xhr.onprogress = (e) => {
-            if (e.lengthComputable) {
-                const percentComplete = (e.loaded / e.total) * 100;
-                onProgress(Math.round(percentComplete));
+        console.log(audioFileUrl);
+        const sound = new Howl({
+            src: [audioFileUrl],
+            sprite: {
+                0: [0, 31000],
+                1: [30000, 31000],
+                2: [60000, 31000],
+                3: [90000, 31000],
+                4: [120000, 31000],
+                5: [150000, 31000],
+                6: [180000, 31000],
+                7: [210000, 31000],
+                8: [240000, 31000],
+                9: [270000, 31000],
+                10: [300000, 31000],
+                11: [330000, 31000],
+                12: [360000, 31000],
+                full: [0, 390000]
             }
+        });
+
+        // Preload audio file
+        const downloadFile = (url, onProgress, onComplete) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.responseType = 'blob';
+
+            xhr.onprogress = (e) => {
+                if (e.lengthComputable) {
+                    const percentComplete = (e.loaded / e.total) * 100;
+                    onProgress(Math.round(percentComplete));
+                }
+            };
+
+            xhr.onload = () => {
+                onComplete(xhr.response);
+            };
+
+            xhr.send();
         };
 
-        xhr.onload = () => {
-            onComplete(xhr.response);
-        };
-
-        xhr.send();
-    };
-
-    downloadFile(
-        audioFileUrl,
-        (progress) => {
-            $('#progress').css('width', progress + '%');
-            if (progress === 100) {
-                $('#downloadStatus').text('Download completed. Click Connect to start.');
-                $('#preloader').fadeOut(500);
-                $('#connect').fadeIn(1000);
+        downloadFile(
+            audioFileUrl,
+            (progress) => {
+                $('#progress').css('width', progress + '%');
+                if (progress === 100) {
+                    $('#downloadStatus').text('Download completed. Click Connect to start.');
+                    $('#preloader').fadeOut(500);
+                    $('#connect').fadeIn(1000);
+                }
+            },
+            (response) => {
+                sound.load();
             }
-        },
-        (response) => {
-            sound.load();
-        }
-    );
-    if(isAdmin) {
-        $('.container').hide();
-        $('#adminDashboard').show();
-         // Enable wake lock
-        $('#adminPlay').on('click',() => {
-            $('#adminPlay').hide();
-            const noSleep = new NoSleep();
-            noSleep.enable();
-            socket.emit('play');
-            sound.play();
-            socket.on('manipulate', (data) => {
-                sound.stereo(data.pan);
-                sound.rate(data.rate);
-            })
-        })
-        setInterval(() => {
-            socket.emit('checkConnection');
-        }, 5000);
-    }
-    else {
-        // Connect to the server and listen for the 'play' event
-        $('#connect').on('click', () => {
-            const noSleep = new NoSleep();
-            noSleep.enable();  // Enable wake lock
-            socket.emit('clientConnected');
-
-            socket.on('play', () => {
-                sound.play();
+        );
+        if (isAdmin) {
+            $('.container').hide();
+            $('#adminDashboard').show();
+            $('#reset').on('click', () => {
+                socket.emit('reset');
+                sound.stop();
+                $('#adminPlay').show();
             });
-            $('#connect').hide();
-            $('#connection').fadeIn(200);
-            socket.on('selected', () => {
-                $('#connection').hide();
-                // Countdown
-
-                var countdownNumber = 5;
-                $('#countdown').text(countdownNumber);
-                $('#countdown').show();
-
-                var countdownInterval = setInterval(function () {
-                    countdownNumber--;
-                    $('#countdown').text(countdownNumber);
-
-                    if (countdownNumber <= 0) {
-                        clearInterval(countdownInterval);
-                        $('#countdown').hide();
-                        $('#circle').show();
-                    }
-                }, 1000);
-                $('#circle').on('touchmove', function (e) {
-                    e.preventDefault();
-
-                    var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-                    var elm = $(this).offset();
-                    var x = touch.pageX - elm.left;
-                    var y = touch.pageY - elm.top;
-                    var w = $(this).width();
-                    var h = $(this).height();
-
-                    // Calculate rate from y position
-                    var rate = y / h;
-                    rate = Math.max(Math.min(rate * 2, 2.0), 0.5);
-
-                    // Calculate pan from x position
-                    var pan = (x / w) * 2 - 1;// Convert x position to range -1 to 1
-                    socket.emit('userControl', {
-                        pan: pan,
-                        rate: rate
-                    });
+            if (test === 'true') {
+                $('#test').hide();
+                $('#main').on('click', () => {
+                    socket.emit('mainAdmin');
                 });
+        }
+           else {
+                $('#main').hide();
+                $('#test').on('click', () => {
+                    socket.emit('testAdmin');
+                });
+            }
+            // Enable wake lock
+            $('#adminPlay').on('click', () => {
+                $('#adminPlay').hide();
+                const noSleep = new NoSleep();
+                noSleep.enable();
+                socket.emit('play');
+                socket.on('playSong', (data) => {
 
-                setTimeout(function () {
-                    socket.emit('userControl', {
-                        pan: -1,
-                        rate: 1
-                    });
-                    $('#circle').hide();
-                    $('#connected').show();
-                }, 15000);
+                    sound.play(data);
+                })
+                socket.on('manipulate', (data) => {
 
-            });
-
-
+                    if (data) {
+                        sound.stereo(data.pan);
+                        sound.rate(data.rate);
+                    }
+                })
+            })
             setInterval(() => {
                 socket.emit('checkConnection');
-            }, 15000);
+            }, 5000);
+        } else {
+            socket.on('refresh', () => {
+                window.location.reload();
+            })
+            // Connect to the server and listen for the 'play' event
+            $('#connect').on('click', () => {
+                const noSleep = new NoSleep();
+                noSleep.enable();  // Enable wake lock
+                socket.emit('clientConnected');
+
+                socket.on('playSongFull', () => {
+                    console.log();
+                    sound.play('full');
+                });
+                $('#connect').hide();
+                $('#connection').fadeIn(200);
+                socket.on('selected', () => {
+                    $('#connection').hide();
+                    // Countdown
+
+                    var countdownNumber = 5;
+                    $('#countdown').text(countdownNumber);
+                    $('#countdown').show();
+
+                    var countdownInterval = setInterval(function () {
+                        countdownNumber--;
+                        $('#countdown').text(countdownNumber);
+
+                        if (countdownNumber <= 0) {
+                            clearInterval(countdownInterval);
+                            $('#countdown').hide();
+                            $('#circle').show();
+                        }
+                    }, 1000);
+
+                    function enableTouch() {
+                        $('#circle').on('touchmove', touchMoveHandler);
+                        setTimeout(function () {
+                            $('#circle').off('touchmove', touchMoveHandler);
+                            socket.emit('userControl', {
+                                pan: 0,
+                                rate: 1
+                            });
+                            setTimeout(enableTouch, 3000); // Re-enable after 3 seconds
+                        }, 3000); // Disable after 3 seconds
+                    }
+
+                    function touchMoveHandler(e) {
+                        e.preventDefault();
+
+                        var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+                        var elm = $(this).offset();
+                        var x = touch.pageX - elm.left;
+                        var y = touch.pageY - elm.top;
+                        var w = $(this).width();
+                        var h = $(this).height();
+
+                        // Calculate rate from y position
+                        var rate = y / h;
+                        rate = Math.max(Math.min(rate * 1.05, 1.25), 0.25);
+
+                        // Calculate pan from x position
+                        var pan = (x / w) * 2 - 1; // Convert x position to range -1 to 1
+                        socket.emit('userControl', {
+                            pan: pan,
+                            rate: rate
+                        });
+                    }
+
+                    enableTouch();
+
+
+                    setTimeout(function () {
+
+                        $('#circle').hide();
+                        $('#connected').show();
+                    }, 15000);
+
+                });
+
+
+                setInterval(() => {
+                    socket.emit('checkConnection');
+                }, 15000);
+            });
+        }
+        socket.on('connectionOK', () => {
+            console.log('Connection is up.');
+
         });
-    }
-   socket.on('connectionOK', () => {
-        console.log('Connection is up.');
-    });
-});
+    })
+
+    socket.on('main', () => {
+            let url = window.location.origin;
+
+            if (isAdmin) {
+                 let params = $.param({
+                     admin: "true",
+                });
+                url += "?" + params;
+            }
+
+            window.location.href = url;
+        })
+        socket.on('test', () => {
+            let url = window.location.origin;
+            let params = $.param({
+                test: "true",
+            });
+            if (isAdmin) {
+                params = $.param({
+                    test: "true", admin: "true",
+                });
+            }
+            url += "?" + params;
+
+            window.location.href = url;
+        })
+
 });
--- server.js
+++ server.js
@@ -13,79 +13,124 @@ app.use(express.static('./'));
 let selectableUsers = {};
 let group = 0;
 let admin = "";
+let test = false;
 io.on('connection', (socket) => {
+    socket.on('reset', () => {
+        test = false;
+        admin = socket.id;
+        selectableUsers = {};
+        io.emit('refresh');
+    })
+    socket.on('mainAdmin', () => {
+        if (admin !== "") {
+            admin = socket.id;
+        }
+        test = false;
+        selectableUsers = {};
+        io.emit('main');
+    })
 
-  socket.on('reset',() => {
-    selectableUsers = {};
-    socket.broadcast.emit('refresh');
-  })
+    socket.on('testAdmin', () => {
+        if (admin !== "") {
+            admin = socket.id;
+        }
+        test = true;
+        selectableUsers = {};
+        io.emit('test');
+    })
+    socket.on('isAdmin', () => {
+        if (admin !== "") {
+            admin = socket.id;
+            console.log("Admin: " + admin);
+        }
+    })
 
-  socket.on('isAdmin',() => {
-    admin = socket.id;
-    console.log("Admin: " + admin);
-  })
-
-  console.log('A user connected: ' + socket.id);
+    console.log('A user connected: ' + socket.id);
 
     socket.on('clientConnected', () => {
-      console.log('Connected user is Ready: ' + socket.id);
+        console.log('Connected user is Ready: ' + socket.id);
 
-      selectableUsers[socket.id] = socket;
+        selectableUsers[socket.id] = socket;
     });
 
 
-  socket.on('userControl',(data) => {
-    io.to(admin).emit('manipulate', data);
-  })
-        socket.on('disconnect', () => {
-    console.log('A user disconnected: ' + socket.id);
-  });
-  socket.on('play', () => {
-    socket.broadcast.emit('play');
+    socket.on('userControl', (data) => {
+        io.to(admin).emit('manipulate', data);
+    })
+    socket.on('disconnect', () => {
+        console.log('A user disconnected: ' + socket.id);
+    });
+    let i = 0;
+    socket.on('play', () => {
+        console.log('Playback started')
+        let intervalId;
+        let counter = 0;
+        let playSongId;
+        io.emit('playSongFull');
+        function playSong() {
+            if (13 > counter) {
 
-    let intervalId;
+                if(test){
+                    console.log('Test started')
+                    socket.emit('playSong', 'full');
+                    clearInterval(playSongId);
+                }
+                else {
+                    console.log('Playback started')
+                    socket.emit('playSong', counter.toString());
+                    counter++;
+                }
+            } else {
+                clearInterval(playSongId);
+            }
+        }
 
-    function selectRandomUser() {
-      if (Object.keys(selectableUsers).length > 0) {
-        let socketIds = Object.keys(selectableUsers);
-        let randomSocketId = socketIds[Math.floor(Math.random() * socketIds.length)];
-        console.log('Selected: ' + randomSocketId);
-        io.to(randomSocketId).emit('selected');
+        function selectRandomUser() {
+            if (counter >= 1 && counter !== 9) {
+                if (Object.keys(selectableUsers).length > 0) {
+                    let socketIds = Object.keys(selectableUsers);
+                    let randomSocketId = socketIds[Math.floor(Math.random() * socketIds.length)];
+                    console.log('Selected: ' + randomSocketId);
+                    io.to(randomSocketId).emit('selected');
 
-        // Remove selected user so they can't be selected again
-        //delete selectableUsers[randomSocketId];
-      } else {
-        // All users have been selected, so stop selecting
-        clearInterval(intervalId);
-      }
-    }
+                    // Remove selected user so they can't be selected again
+                    delete selectableUsers[randomSocketId];
+                }
+            } else {
+                // All users have been selected, so stop selecting
+                clearInterval(intervalId);
+            }
+        }
 
-    // Select first user immediately
-    selectRandomUser();
-
-    // Schedule selection of next user every 15 seconds
-    intervalId = setInterval(selectRandomUser, 15000);
-  });
-  socket.on('checkConnection', () => {
-    socket.emit('connectionOK');
-  });
+        // Select first user immediately
+        selectRandomUser();
+        playSong();
+        // Schedule selection of next user every 15 seconds
+        intervalId = setInterval(selectRandomUser, 15000);
+        playSongId = setInterval(playSong, 30000);
+    });
+    socket.on('checkConnection', () => {
+        socket.emit('connectionOK');
+    });
 
 });
+
 function getLocalIP() {
-  const interfaces = os.networkInterfaces();
-  for (const ifaceName in interfaces) {
-    const iface = interfaces[ifaceName];
-    for (const alias of iface) {
-      if (alias.family === 'IPv4' && !alias.internal) {
-        return alias.address;
-      }
+    const interfaces = os.networkInterfaces();
+    for (const ifaceName in interfaces) {
+        const iface = interfaces[ifaceName];
+        for (const alias of iface) {
+            if (alias.family === 'IPv4' && !alias.internal) {
+                return alias.address;
+            }
+        }
     }
-  }
-  return '127.0.0.1';
+    return '127.0.0.1';
 }
+
 const PORT = process.env.PORT || 3000;
 const localIP = getLocalIP();
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://${localIP}:${PORT}`);
+    console.log(`Server is running on http://${localIP}:${PORT}`);
 });
