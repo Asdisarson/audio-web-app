d1ac8f4f746ef44eae065cf3f3a9340a52c4eca4
egill <egill@islandsvefir.is>
2023-05-17 11:09:26 +0000

    extra done

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ function getLocalIP() {
     return '127.0.0.1';
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const localIP = getLocalIP();
 
 server.listen(PORT, () => {
